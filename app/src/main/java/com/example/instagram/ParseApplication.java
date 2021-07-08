6ac870c7a8e5d6a7200b02105781109d1aea4a21
Diego Torres <djtorres02@fb.com>
2021-07-08 16:53:28 -0500

    Adds ability to post to parse

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -2,13 +2,18 @@ package com.example.instagram;
 
 import android.app.Application;
 
+import com.example.instagram.models.Post;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Registers the post model to our app
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("jOePW501qKwxoyyWt0XZKDd44C9kLiAokSjNmxGz")
                 .clientKey("0HZvy1ToEcHhMzruY7GuEFkNfCYOTct1fD4HE2co")
---
+++ app/src/main/java/com/example/instagram/models/Post.java
@@ -0,0 +1,42 @@
+package com.example.instagram.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.sql.Date;
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_USER = "user";
+    public static final String KEY_CREATEDAT = "createdAt";
+
+    // Make getters and setters
+    public String getDescription(){
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description){
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile image){
+        put(KEY_IMAGE, image);
+    }
+
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
+}
